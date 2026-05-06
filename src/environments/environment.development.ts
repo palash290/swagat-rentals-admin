89749cfd18f48d8344f2c6d69051fe46528b7e38
Palash <palashjain.ctinfotech@gmail.com>
2026-05-06 12:32:43 +0530

    new changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
       production: false,
       // local url
-      baseUrl: 'http://192.168.1.46:8011/api/',
+      baseUrl: 'http://192.168.1.42:8011/api/',
 
       // production url or live url
       // baseUrl: 'http://187.124.99.24:8010/api/'
