89f0e797a883134e3d7d0add26160d3afda93e0a
govindkumawat123 <govindkumawat.github@gmail.com>
2026-05-08 17:37:36 +0530

    new changes

--- public/js/main.js
+++ public/js/main.js
@@ -12,4 +12,33 @@ $(document).ready(function () {
   $(".ct_close_sidebar").click(function () {
     $("main").removeClass("ct_show");
   });
+
+  $(".ct_approach_slider").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0,
+      },
+      767: {
+        items: 1,
+        stagePadding: 0,
+      },
+      991: {
+        items: 2,
+      },
+    },
+  });
+
+  $(".ct_next_btn").click(function () {
+    $(".ct_approach_slider").trigger("next.owl.carousel");
+  });
+
+  $(".ct_prev_btn").click(function () {
+    $(".ct_approach_slider").trigger("prev.owl.carousel");
+  });
 });
